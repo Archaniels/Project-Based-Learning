72912b1c69d01a5b37d97896e290bee18aac5698
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-05-23 13:00:30 +0700

    Inisialisasi Projek

---
+++ program.py
@@ -0,0 +1,5 @@
+#berikut pengumuman mengenai PROJECT-BASED.
+#1. carilah dataset gratis, source bebas (contoh: kaggle)
+#2. ⁠domain AI tidak dibatasi, bisa computer vision, NLP, atau data science.
+#3. ⁠metode/algoritma dibebaskan, pilihlah yang cocok dan bisa digunakan untuk dataset yang kalian gunakan.
+#4. ⁠sisa ketentuan ada di pdf berikut
