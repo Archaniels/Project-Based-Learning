ff8dabe5ae05979dbd3259bb5eb503ab45479c14
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-05-31 18:40:50 +0700

    Update program.py

--- program.py
+++ program.py
@@ -11,7 +11,7 @@
 ## Sisa ketentuan ada di PDF berikut
 
 ## =========== PENJELASAN TUGAS ===========
-## 1. Diberikan sebuah dataset berformat .CSV yang berisi data-data yang berkaitan dengan data housing di King County, USA. Dataset ini berisi informasi mengenai harga jual rumah beserta fitur-fiturnya, seperti jumlah kamar tidur, ukuran rumah, lokasi, dan sebagainya.
+## 1. Diberikan sebuah dataset berformat .CSV yang berisi data-data yang berkaitan dengan data.
 ## 2. Tugas adalah untuk menerapkan domain AI yang digunakan, serta metode/algoritma yang telah dipelajari di Learning.
 ## 3. Domain AI yang digunakan adalah Machine Learning, dengan subdomain adalah regression.
 ## 4. Metode algorithma yang digunakan adalah Regresi Linear, karena cocok untuk memprediksi harga rumah, berdasarkan fitur numerik dan kategori.
@@ -29,7 +29,7 @@ import pandas as pd ## untuk memproses file dataset .CSV
 import numpy as np ## untuk perhitungan
 import matplotlib.pyplot as plt ## untuk visualisasi data mungkin?
 
-filepath = "C:\Users\sxpix\Documents\~~~ CODES\Visual Studio 2022\Project-Based-Learning\Project-Based-Learning\kc_house_data.csv"
+filepath = "C:\Users\sxpix\Documents\~~~ CODES\Visual Studio 2022\Project-Based-Learning\Project-Based-Learning\"
 
 def load_dataset(filepath):
     
