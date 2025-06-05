32cda87c43a8a5d69a9a7ef7ef7e6132f8166665
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-06-05 10:46:48 +0700

    [NEW] Penambahan logika untuk preprocessing, pemisahan data training & testing, dan main

--- program.py
+++ program.py
@@ -29,7 +29,54 @@ import pandas as pd ## untuk memproses file dataset .CSV
 import numpy as np ## untuk perhitungan
 import matplotlib.pyplot as plt ## untuk visualisasi data mungkin?
 
-filepath = "C:\Users\sxpix\Documents\~~~ CODES\Visual Studio 2022\Project-Based-Learning\Project-Based-Learning\"
 
-def load_dataset(filepath):
+def preprocessing():
+    filepath = r"C:\Users\sxpix\Documents\~~~ CODES\Visual Studio 2022\Project-Based-Learning\ai_dev_productivity.csv"
+
+    # membaca file dataset .CSV
+    df = pd.read_csv(filepath)
+
+    # Memisahkan fitur dan label
+    X = df.drop(columns=['task_success'])  # Semua kolom kecuali target
+    y = df['task_success']  # Kolom target
+
+    # Mengubah ke array numpy agar lebih mudah diproses secara manual
+    X = X.to_numpy()
+    y = y.to_numpy()
+
+    return X, y
+
+# Membagi data ke training dan testing set (80% train & 20% test / 400 data untuk training & 100 data untuk testing)
+def pemisahan_testing_training(X, y, test_size=0.2, seed=42):
+    np.random.seed(seed)
+    indices = np.random.permutation(len(X))
+    jumlah_test = int(len(X) * test_size)
     
+    test_index = indices[:jumlah_test]
+    training_index = indices[jumlah_test:]
+    
+    X_train, X_test = X[training_index], X[test_index]
+    y_train, y_test = y[training_index], y[test_index]
+    
+    return X_train, X_test, y_train, y_test
+
+def entropy():
+
+def information_gain():
+
+def struktur_tree():
+
+
+def main():
+    # Panggil preprocessing dan ambil data
+    X, y = preprocessing()
+
+    # Bagi data
+    X_train, X_test, y_train, y_test = pemisahan_testing_training(X, y)
+
+    # Tampilkan ukuran data
+    print("Jumlah data training:", len(X_train))
+    print("Jumlah data testing:", len(X_test))
+
+if __name__ == "__main__":
+    main()
